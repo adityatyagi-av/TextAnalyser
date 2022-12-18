384aae744bea2322dcebadac5d4d878d5ba3debc
Aditya Tyagi <adityatyagiav1@gmail.com>
2022-12-18 19:00:50 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 
 import './App.css';
+import Navbar from './Navbars';
 
 function App() {
   return (
     <div className="App">
-     <nav className="navbar">
-      <ul>
-        <li>home</li>
-        <li>about</li>
-        <li>service</li>
-        </ul>
-     </nav>
      <div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptates qui veritatis at voluptas molestiae dolore dolorem quo reprehenderit eius. Ab deserunt aut iste quas laudantium saepe eius? Qui veritatis quisquam odio minus assumenda alias quidem sed ex maiores delectus voluptate quasi atque, eum dolorum repellat totam perspiciatis vitae voluptatum architecto dolorem. Voluptatibus harum voluptatum aut sequi dicta, perferendis molestiae corporis accusantium similique nemo officiis cumque, facilis possimus, inventore praesentium! Inventore, tempore temporibus quos sequi itaque fugiat assumenda omnis voluptatibus, laborum eaque quisquam a beatae vero incidunt reiciendis? Dicta inventore blanditiis eveniet, corrupti atque perspiciatis itaque cum incidunt fugiat temporibus.</p>
+      <Navbar />
+      <img src="" alt="" />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptates qui veritatis at voluptas molestiae dolore dolorem quo reprehenderit eius. Ab deserunt aut iste quas laudantium saepe eius? Qui veritatis quisquam odio minus assumenda alias quidem sed ex maiores delectus voluptate quasi atque, eum dolorum repellat totam perspiciatis vitae voluptatum architec to dolorem. Voluptatibus harum voluptatum aut sequi dicta, perferendis molestiae corporis accusantium similique nemo officiis cumque, facilis possimus, inventore praesentium! Inventore, tempore temporibus quos sequi itaque fugiat assumenda omnis voluptatibus, laborum eaque quisquam a beatae vero incidunt reiciendis? Dicta inventore blanditiis eveniet, corrupti atque perspiciatis itaque cum incidunt fugiat temporibus.</p>
      </div>
     </div>
   );
