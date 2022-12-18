742578ce5eb0b76712f98e3ead2ffdac4a115c96
Aditya Tyagi <adityatyagiav1@gmail.com>
2022-12-18 17:32:53 +0530

    React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <nav className="navbar">
+      <ul>
+        <li>home</li>
+        <li>about</li>
+        <li>service</li>
+        </ul>
+     </nav>
+     <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptates qui veritatis at voluptas molestiae dolore dolorem quo reprehenderit eius. Ab deserunt aut iste quas laudantium saepe eius? Qui veritatis quisquam odio minus assumenda alias quidem sed ex maiores delectus voluptate quasi atque, eum dolorum repellat totam perspiciatis vitae voluptatum architecto dolorem. Voluptatibus harum voluptatum aut sequi dicta, perferendis molestiae corporis accusantium similique nemo officiis cumque, facilis possimus, inventore praesentium! Inventore, tempore temporibus quos sequi itaque fugiat assumenda omnis voluptatibus, laborum eaque quisquam a beatae vero incidunt reiciendis? Dicta inventore blanditiis eveniet, corrupti atque perspiciatis itaque cum incidunt fugiat temporibus.</p>
+     </div>
     </div>
   );
 }
