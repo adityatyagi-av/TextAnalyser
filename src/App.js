eb8eff7bfc97c92f53c3ed7ff3c1b653bcb6e2fc
Aditya Tyagi <adityatyagiav1@gmail.com>
2022-12-19 20:17:45 +0530

    Props concept

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 
 import './App.css';
-import Navbar from './Navbars';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
      <div>
-      <Navbar />
-      <img src="" alt="" />
+     
+      <Navbar title="TextUtils23" hometext="Home" abouttext="About us"/>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptates qui veritatis at voluptas molestiae dolore dolorem quo reprehenderit eius. Ab deserunt aut iste quas laudantium saepe eius? Qui veritatis quisquam odio minus assumenda alias quidem sed ex maiores delectus voluptate quasi atque, eum dolorum repellat totam perspiciatis vitae voluptatum architec to dolorem. Voluptatibus harum voluptatum aut sequi dicta, perferendis molestiae corporis accusantium similique nemo officiis cumque, facilis possimus, inventore praesentium! Inventore, tempore temporibus quos sequi itaque fugiat assumenda omnis voluptatibus, laborum eaque quisquam a beatae vero incidunt reiciendis? Dicta inventore blanditiis eveniet, corrupti atque perspiciatis itaque cum incidunt fugiat temporibus.</p>
      </div>
     </div>
