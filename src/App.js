752d53766cabb0b07dbc2e5efa5c9afeb82bd59e
Aditya Tyagi <adityatyagiav1@gmail.com>
2022-12-20 18:37:07 +0530

    minor modifications

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import Textfrom from './components/Textfrom';
+// import Textfrom from './components/Textfrom';
 function App() {
   return (
     <div className="App">
      <div>
      
       <Navbar title="TextUtils23" hometext="Home" abouttext="About us"/>
-      <div className="container my-3">
+      {/* <div className="container my-3">
       <Textfrom heading= "Enter the text to analyse"/>
-      </div>
-      
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptates qui veritatis at voluptas molestiae dolore dolorem quo reprehenderit eius. Ab deserunt aut iste quas laudantium saepe eius? Qui veritatis quisquam odio minus assumenda alias quidem sed ex maiores delectus voluptate quasi atque, eum dolorum repellat totam perspiciatis vitae voluptatum architec to dolorem. Voluptatibus harum voluptatum aut sequi dicta, perferendis molestiae corporis accusantium similique nemo officiis cumque, facilis possimus, inventore praesentium! Inventore, tempore temporibus quos sequi itaque fugiat assumenda omnis voluptatibus, laborum eaque quisquam a beatae vero incidunt reiciendis? Dicta inventore blanditiis eveniet, corrupti atque perspiciatis itaque cum incidunt fugiat temporibus.</p>
-     </div>
+      </div> */}
+      <About/>
+       </div>
     </div>
   );
 }
--- src/components/Textfrom.js
+++ src/components/Textfrom.js
@@ -2,24 +2,43 @@ import React from 'react'
 import {useState} from 'react';
 export default function Textfrom(props) {
   const[text,newText]=useState('Enter text here');
-  let updateText =() =>{
+  let updateText1 =() =>{
    let rex=text.toUpperCase();
    newText(rex);
    
    
   }
+
+  let updateText2 =() =>{
+    let rex=text.toLowerCase();
+    newText(rex);
+    
+    
+   }
+
   const handleOnChange =(event) =>{
     console.log("On change");
     newText(event.target.value);
   }
   
   return (
-    <div>
+    <>
+    <div className="container">
         <h1>{props.heading}</h1>
     <div className="mb-3">
      <textarea className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
     </div>
-    <button className="btn btn-primary" onClick={updateText}>Convert to upper case</button>
+    <button className="btn btn-primary" onClick={updateText1}>Convert to upper case</button>
+    <button className="btn btn-primary mx-3" onClick={updateText2}>Convert to lower case</button>
+   
     </div>
+    <div className="container">
+      <h1 className='my-4'>Your Text Summary</h1>
+      <p>Your text have {text.split(' ').length} words and {text.length} characters</p>
+      <p>{0.008* text.split(' ').length} Minutes read</p>
+      <h2 className="my-3">Preview</h2>
+      <p>{text}</p>
+    </div>
+    </>
   )
 }
